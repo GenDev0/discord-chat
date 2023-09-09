52a7d2ac6b4fc04f33d31a0646897c141c2f7b09
GenDev0 <chebbi.ahmed.info@gmail.com>
2023-09-09 12:18:04 +0100

    folder structure

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,5 @@
+interface RegisterPageProps {}
+const RegisterPage = (props: RegisterPageProps) => {
+  return <div>RegisterPage</div>;
+};
+export default RegisterPage;
---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return <div className='bg-red-500 h-full'>{children}</div>;
+};
+export default AuthLayout;
