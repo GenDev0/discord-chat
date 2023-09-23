ac7607e011dde067eace77392c200ef4da4ecef4
GenDev0 <chebbi.ahmed.info@gmail.com>
2023-09-23 18:05:44 +0100

    feat : add welcomeChat and QueryProvider from transtack

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -6,6 +6,7 @@ import { currentProfile } from "@/lib/currentProfile";
 
 import ChatInput from "@/components/chat/chat-input";
 import ChatHeader from "@/components/chat/chat-header";
+import ChatMessages from "@/components/chat/chat-messages";
 
 type ChannelIdPageProps = {
   params: {
@@ -46,7 +47,20 @@ const ChannelIdPage = async ({
         serverId={channel.serverId}
         type={"channel"}
       />
-      <div className='flex-1'>Future Messages...!</div>
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type={"channel"}
+        apiUrl={"/api/messages"}
+        socketUrl={"/api/socket/messages"}
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey={"channelId"}
+        paramValue={channel.id}
+      />
       <ChatInput
         name={channel.name}
         type={"channel"}
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils";
 import ModalProvider from "@/components/providers/modal-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -32,8 +33,10 @@ export default function RootLayout({
             storageKey='discord-theme'
           >
             <SocketProvider>
-              <ModalProvider />
-              {children}
+              <QueryProvider>
+                <ModalProvider />
+                {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
