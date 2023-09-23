8b107bba44035dd961e6f7414a8a2229df3b498b
GenDev0 <chebbi.ahmed.info@gmail.com>
2023-09-23 12:29:22 +0100

    feat : add chatHeader and toggle Menu

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,16 +1,50 @@
+import { redirect } from "next/navigation";
+import { redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/currentProfile";
+
+import ChatHeader from "@/components/chat/chat-header";
+
 type ChannelIdPageProps = {
   params: {
     serverId: string;
     channelId: string;
   };
 };
-const ChannelIdPage = ({
+const ChannelIdPage = async ({
   params: { serverId, channelId },
 }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect(`/`);
+  }
+
   return (
-    <div>
-      serverId : {serverId} <br />
-      ChannelId : {channelId}
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type={"channel"}
+      />
     </div>
   );
 };
