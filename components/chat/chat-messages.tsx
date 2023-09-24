834e01b8c26950d846f9da5a561300cf339bdbe7
GenDev0 <chebbi.ahmed.info@gmail.com>
2023-09-24 15:46:35 +0100

    feat : add Edit message

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { Fragment } from "react";
+import { format } from "date-fns";
 import { Member } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
 
+import { MessageWithMemberWithProfile } from "@/types";
+
 import ChatWelcome from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
-import { MessageWithMemberWithProfile } from "@/types";
+import ChatItem from "./chat-item";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 interface ChatMessagesProps {
   name: string;
@@ -71,7 +76,19 @@ const ChatMessages = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={`page-${i}`}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>{message.content}</div>
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
